dcd80b958ee1ca66aeb09f621e2bf0fbaca594c1
josetoujours <26528026+josetoujours@users.noreply.github.com>
2022-11-30 14:11:34 +0100

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import App from './App';
+import App from './components/App/App';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+// Path: src/index.js
+// Compare this snippet from src/components/Business/Business.js:
+// import React from 'react';
+
+// import './Business.css';
+
+
+
+
+
